d25b3f98eeee022548dd0d13cc10cd583f1eab43
inwerpsel <pieter.vincent1988@gmail.com>
2021-05-17 18:14:01 +0200

    Local storage namespace data attr

--- src/useLocalStorage.js
+++ src/useLocalStorage.js
@@ -1,14 +1,20 @@
 import { useEffect, useState } from 'react';
 
+const base = `${document.documentElement.dataset.base}`;
+const [,rawNs] = base.split(window.location.origin);
+const namespace = (rawNs || '').replace(/^\//, '').replace(/\/$/, '');
+
 export const useLocalStorage = (key, defaultValue) => {
+  const scopedKey = !namespace ? key : `${namespace || ''}${key}`;
+
   const [value, setValue] = useState(() => {
-    const stored = localStorage.getItem(key);
+    const stored = localStorage.getItem(scopedKey);
 
     return stored === null ? defaultValue : stored;
   });
 
   useEffect(() => {
-    localStorage.setItem(key, value);
+    localStorage.setItem(scopedKey, value);
   }, [value]);
 
   return [
