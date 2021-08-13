e7c2fd8bb80a46c8dedabc7354e27b1e2b748938
inwerpsel <pieter.vincent1988@gmail.com>
2021-08-13 14:14:42 +0200

    Add explanation about using different defaults

--- src/getAllDefaultValues.js
+++ src/getAllDefaultValues.js
@@ -1,7 +1,9 @@
 export const getAllDefaultValues = allVars => {
   const fromAvailableVars = allVars.reduce((values, cssVar) => {
-    // All should use the default value, but can't be guaranteed. If they're different it's hard to tell which it
-    // should be. Just use the first one. No usages is not possible in theory but check existence anyway.
+    // All should use the default value (as a best practice), but can't be guaranteed. If they're different it's hard to
+    // tell which it should be. Just use the first one. No usages is not possible in theory but check existence anyway.
+    // I'm hesitant to support the case of the same variable having different default values. It seems like a bad
+    // practice. Instead the CSS should use different variables for these cases.
     const supposedDefaultValue = cssVar.usages[0]?.defaultValue;
 
     return {
