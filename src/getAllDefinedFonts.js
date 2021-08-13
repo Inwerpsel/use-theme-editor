b7f980a20ee720e9fc9f93c9199b9d6504642f74
inwerpsel <pieter.vincent1988@gmail.com>
2021-08-13 16:03:01 +0200

    Fix collecting variants

--- src/getAllDefinedFonts.js
+++ src/getAllDefinedFonts.js
@@ -4,7 +4,7 @@ const GOOGLE_FONTS_URL = 'https://fonts.googleapis.com';
 
 // Because of a security mechanism in browsers, it's not possible to directly access the rules on a stylesheet of
 // another domain. This is only a minor nuisance because it's dead simple to fetch the sheet, add its contents to a new
-// style node and read that one. It's even returned from browser cache so pretty fast actually.
+// style node and read that one. It's even returned from browser cache, so pretty fast actually.
 const getGoogleSheetRules = async href => {
   const id = href.replace(/[^\w]/g, '');
   const inDom = document.querySelector('style#' + id);
@@ -30,17 +30,20 @@ const extractFonts = async (fonts, sheet) => {
 
   return await fontFaces.reduce(async (fonts, {style: {fontFamily, fontWeight, fontStyle}}) => {
 
+    const previousFonts = await fonts;
+    const previousVariants = previousFonts[fontFamily]?.variants || {};
+
     return {
-      ...await fonts,
+      ...previousFonts,
       [fontFamily]: {
         fontFamily,
-        variants: [
-          ...fonts[fontFamily]?.variants || [],
-          {
+        variants: {
+          ...previousVariants,
+          [`${fontWeight || 'normal'}|${fontStyle || 'normal'}`]: {
             fontWeight: fontWeight || 'normal',
             fontStyle: fontStyle || 'normal',
           }
-        ]
+        }
       }
     };
   }, fonts);
