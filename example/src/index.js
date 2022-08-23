a0d403374fe67bf9a8c3b70d551fee5d0d2397b4
Pieter Vincent <pieter.vincent1988@gmail.com>
2022-08-23 12:33:58 +0200

    Mock server in localStorage in example

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,21 @@
 import { setupThemeEditor } from "../../src/initializeThemeEditor";
 
+const KEY = 'all-the-themes';
 
 setupThemeEditor({
     serverThemes: {
-        fetchThemes: () => { console.log('fetch') },
-        uploadTheme: () => { console.log('upload') },
-        deleteTheme: () => { console.log('delete') },
+        fetchThemes: () => {
+            return JSON.parse(localStorage.getItem(KEY));
+        },
+        uploadTheme: (name, theme) => {
+            const themes = JSON.parse(localStorage.getItem(KEY)) || {};
+            themes[name] = theme;
+            localStorage.setItem(KEY, JSON.stringify(themes, null, 2) )
+        },
+        deleteTheme: (name) => {
+            const themes = JSON.parse(localStorage.getItem(KEY)) || {};
+            delete themes[name];
+            localStorage.setItem(KEY, JSON.stringify(themes, null, 2) )
+        },
     },
 });
