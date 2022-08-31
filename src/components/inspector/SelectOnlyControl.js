553930113f50fecc51def11cfec3551f8027493f
Pieter Vincent <pieter.vincent1988@gmail.com>
2022-08-31 10:15:16 +0200

    Add some flexbox keywords

--- src/components/inspector/SelectOnlyControl.js
+++ src/components/inspector/SelectOnlyControl.js
@@ -13,6 +13,33 @@ const lists = {
         'safe center',
         'unsafe center',
     ],
+    'flex-direction': [
+        'row',
+        'row-reverse',
+        'column',
+        'column-reverse',
+    ],
+    'justify-content': [
+        'center',
+        'start',
+        'end',
+        'flex-start',
+        'flex-end',
+        'left',
+        'right',
+        'normal',
+        'space-between',
+        'space-around',
+        'space-evenly',
+        'stretch',
+        'safe center',
+        'unsafe center',
+    ],
+    'flex-wrap': [
+        'nowrap',
+        'wrap',
+        'wrap-reverse',
+    ],
 };
 
 export function selectOnlyOptions(cssVar) {
