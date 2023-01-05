1f7d7f74c64716a5ee6c77d364c3da7ceb094a9a
Pieter Vincent <pieter.vincent1988@gmail.com>
2023-01-05 16:58:51 +0100

    Groups and alias history action previews

--- src/renderSelectedVars.js
+++ src/renderSelectedVars.js
@@ -1,22 +1,37 @@
 import React from 'react';
-import {render} from 'react-dom';
 import { createRoot} from 'react-dom/client';
 import { COLOR_VALUE_REGEX, GRADIENT_REGEX } from './components/properties/ColorControl';
-import {ThemeEditor} from './components/ThemeEditor';
+import {prevGroups, ThemeEditor} from './components/ThemeEditor';
 import { SharedActionHistory } from './hooks/useResumableReducer';
 
 let root;
 const size = 18;
 
-
 export const previewComponents = {
+  OPEN_GROUPS: ({ action: groups }) => (
+    <div>
+      <pre className="monospace-code" style={{ float: 'left' }}>
+        {Object.entries(groups).map(([k, v]) => [
+          k,
+          <b style={{float: 'right', color: 'white', background: 'black'}}>{v ? 'open' : 'close'}</b>,
+        ])}
+      </pre>
+    </div>
+  ),
+  'inspected-index': ({action: index}) => <div>
+    Inspect
+    <pre className="monospace-code">{prevGroups [index][0]?.label}</pre>
+  </div>,
+  // 'inspected-index': ({action: index}) => '',
   THEME_EDITOR: {
     set: ({ payload: { scope, name, value } }) => (
       <div>
         {scope && <pre className="monospace-code">{scope}</pre>}
         <br />
         <b>{name}</b> =
-        {(COLOR_VALUE_REGEX.test(value) || GRADIENT_REGEX.test(value)) && (
+        {(COLOR_VALUE_REGEX.test(value) ||
+          GRADIENT_REGEX.test(value) ||
+          /var\(/.test(value)) && (
           <span
             style={{
               width: size,
@@ -41,6 +56,31 @@ export const previewComponents = {
         {scope && <pre className="monospace-code">{scope}</pre>}
         <br />
         <b>{name}</b> = default
+      </div>
+    ),
+    createAlias: ({ payload: { name, value } }) => (
+      <div>
+        Alias
+        <br />
+        {(COLOR_VALUE_REGEX.test(value) ||
+          GRADIENT_REGEX.test(value) ||
+          /var\(/.test(value)) && (
+          <span
+            style={{
+              width: size,
+              height: size,
+              border: '1px solid black',
+              borderRadius: '6px',
+              backgroundImage: `${value}`,
+              backgroundColor: `${value}`,
+              backgroundRepeat: `no-repeat`,
+              backgroundSize: 'cover',
+              display: 'inline-block',
+              textShadow: 'white 0px 10px',
+            }}
+          ></span>
+        )}
+        {value} = <b>{name}</b>
       </div>
     ),
   },
