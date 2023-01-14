cff3f10890d11356c748e160d7bfc963b8de3f50
Pieter Vincent <pieter.vincent1988@gmail.com>
2023-01-14 15:48:26 +0100

    Convert state index to TS, declare as const
    No other TS needed because generics handle it based on arguments

--- src/state/index.ts
+++ src/state/index.ts
@@ -20,6 +20,6 @@ export const use = {
   scales: () => useResumableLocalStorage('responsive-scales', {}),
   uiArrangement: () => useResumableLocalStorage('panel-rearrangements', {}),
   propertyFilter: () => useResumableLocalStorage('property-filter', 'all'),
-};
+} as const;
 
 export const get = getters(use);
