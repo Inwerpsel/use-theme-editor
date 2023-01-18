4944158e1743679a01374604a18ebea1114bdb69
Pieter Vincent <pieter.vincent1988@gmail.com>
2023-01-18 14:37:03 +0100

    Different global store for non resumable state
    See code comment
    Allows to call hook multiple times and get updates everywhere
    Update tsconfig so that Sets work
    they were horribly broken with the previous config
    .values() would not iterate but also no error

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useInsertionEffect, useState } from 'react';
 import {getLocalStorageNamespace} from '../functions/getLocalStorageNamespace';
 import { useResumableState } from './useResumableReducer';
 
@@ -21,30 +21,76 @@ function apply(type, value) {
   }
   }
 }
+type SetsOfDispatchers = {
+  [key: string]: Set<(...arg: any) => void>
+}
 
+const readProxy = {};
+// const dispatchers = {};
+const setters = {};
+const initializers = {};
+const dispatchers: SetsOfDispatchers = {};
+
+// This function takes a fundamentally different approach to global state subscriptions than useSyncExternalStore.
+// Components add their local state dispatchers to a set. The wrapping dispatcher then just iterates this set with any new value.
+// It may seem odd to have both approaches in the same repo, but this also seems a simple approach that so far works just fine.
+// Maybe both approaches have their own merits, or one is clearly better. Honestly I can't tell at the moment.
 export function useLocalStorage<T>(key: string, defaultValue: T): [T, (arg: T) => void] {
   const scopedKey = getLocalStorageNamespace() + key;
   const type = typeof defaultValue;
   const isObject = type === 'object';
+  const isRepeat = initializers.hasOwnProperty(key);
 
-  const [value, setValue] = useState(() => {
-    const stored = localStorage.getItem(scopedKey);
-    if (stored === null) {
-      return defaultValue;
+  const initializer = isRepeat ? initializers[key] : () => {
+    let storedOrDefaultValue;
+
+    if (readProxy.hasOwnProperty(key)) {
+      storedOrDefaultValue = readProxy[key];
+    } else {
+      // Do first time setup for key.
+      const stored = localStorage.getItem(scopedKey);
+      if (stored === null) {
+        return defaultValue;
+      }
+      const storedValue = apply(type, stored);
+      readProxy[key] = storedValue;
+      dispatchers[key] = new Set();
+
+      setters[key] = arg => {
+        const newValue = typeof arg === 'function' ? arg(value) : arg;
+        localStorage.setItem(
+          scopedKey,
+          !isObject ? newValue : JSON.stringify(newValue),
+        );
+        readProxy[key] = newValue;
+        for (const setValue of dispatchers[key].values()) {
+          console.log(setValue);
+          setValue(newValue);
+        }
+      }
+
+      storedOrDefaultValue = storedValue;
     }
-    return apply(type, stored);
-  });
+
+    return storedOrDefaultValue;
+  }
+  if (!isRepeat) {
+    initializers[key] = initializer;
+  }
+
+  const [value, setValue] = useState(initializer);
+
+  // Insertion effect as the dispatchers should be usable (or gone) for all subsequent effects.
+  useInsertionEffect(() => {
+    dispatchers[key].add(setValue);
+    return () => {
+      dispatchers[key].delete(setValue);
+    }
+  }, []);
 
   return [
     value,
-    arg => {
-      const newValue = typeof arg === 'function' ? arg(value) : arg;
-      localStorage.setItem(
-        scopedKey,
-        !isObject ? newValue : JSON.stringify(newValue)
-      );
-      setValue(newValue);
-    }
+    setters[key],
   ];
 };
 
