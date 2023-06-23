6714a8d99f4fc77f4eb1c001c66001b7705976f3
Pieter Vincent <pieter.vincent1988@gmail.com>
2023-06-23 15:10:16 +0200

    Improve signals example

--- src/components/_examples/SignalExample.tsx
+++ src/components/_examples/SignalExample.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from "react";
+import React, { Fragment, useState } from "react";
 import { $ } from "../../state";
 import { Checkbox } from "../controls/Checkbox";
 
@@ -6,17 +6,19 @@ function TimeAtRender() {
     return <time>{Date.now()}</time>
 }
 
+// I left console logging so that it's easier to inspect the rerender behavior by uncommenting them.
+
 // Unaware component level 2.
 // The component uses the signal as a primitve value in its own logic.
 function FormatProp({prop}) {
-    console.log('FormatProp');
+    // console.log('FormatProp');
     return `The prop says "${prop}"`;
 }
 
 // Unaware component level 2.
 // Since this one only puts attr in JSX, it won't itself be rerendered.
-function FormatPropJSX({prop}) {
-    console.log('FormatPropJSX');
+function FormatPropJSX({prop}: {prop: string}) {
+    // console.log('FormatPropJSX');
     return <Fragment>The prop says "{prop}"</Fragment>;
 }
 
@@ -27,13 +29,16 @@ function FormatPropJSX({prop}) {
 // Since the attr is always the same object, this component will never
 // rerender.
 function INCOMPATIBLE__FormatProp({attr}) {
-    console.log('INCOMPATIBLE__FormatProp');
-    return attr === 'all' ? 'The prop is equal to "all"' : 'The prop is not equal to "all"';
+    // console.log('INCOMPATIBLE__FormatProp');
+    return <span>
+        {attr === 'all' ? 'The prop is equal to "all"' : 'The prop is not equal to "all"'}
+    </span>
 }
 
 // Unaware component level 1.
 function ShowProp({prop, area}) {
     const areaDouble = area * 2;
+    // console.log(prop, area);
 
     return <div>
         <span>
@@ -43,7 +48,7 @@ function ShowProp({prop, area}) {
             <br />
             <b>Expected to not work: </b>
             <br />
-            <INCOMPATIBLE__FormatProp prop={prop} />
+            <INCOMPATIBLE__FormatProp attr={prop} />
         </span>
         <div>
             {/* <input type="text" value={$.annoyingPrefix} /> */}
@@ -62,16 +67,12 @@ export function SignalExample() {
     return <div>
         <h2>Signal example</h2>
         <p>
-            The current area is {$.width} * {$.height} = {$.area}.
+            The current area is {$.width} * {$.height} = {$.area}. * 2 = {$.areaDoubled}
             This string was rendered at <TimeAtRender />, yet the values are always up to date.
         </p>
         <p>
             Area without memo: {$.areaNomemo}
         </p>
-        <p>
-            Twice the area: {$.areaDoubled}
-        </p>
-
         <h3>Get value conditionally</h3>
         <p>
             <Checkbox controls={[on, setOn]}>memo</Checkbox>
