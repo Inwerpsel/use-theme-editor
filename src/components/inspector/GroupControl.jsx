bd0fe32c2237484187b821a942b609db46cc4c27
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-01-23 15:13:19 +0100

    Reduce size of open groups state

--- src/components/inspector/GroupControl.jsx
+++ src/components/inspector/GroupControl.jsx
@@ -43,7 +43,12 @@ export const GroupControl = props => {
     setOpenGroups,
   } = useContext(ThemeEditorContext);
 
-  const toggleGroup = id => setOpenGroups({...openGroups, [id]: !openGroups[id]});
+  const toggleGroup = id => {
+    // Todo: further reduce stored size, perhaps use different key.
+    const {[id]: wasOpen, ...other} = openGroups;
+    const newGroups = wasOpen ? other : {...other, [id]: true};
+    setOpenGroups(newGroups);
+  };
 
   const groupColors = useMemo(() => {
     return vars.reduce((colorVars, someVar) => {
@@ -93,129 +98,133 @@ export const GroupControl = props => {
 
   const isOpen = !!openGroups[group.label];
 
-  return <li className={'var-group'} key={label} style={{marginBottom: '12px'}}>
-    <div
-      onMouseEnter={() => {
-        if (element && element.classList) {
-          addHighlight(element);
-          return;
-        }
+  return (
+    <li className={'var-group'} key={label} style={{marginBottom: '12px'}}>
+      <div
+        onMouseEnter={() => {
+          if (element && element.classList) {
+            addHighlight(element);
+            return;
+          }
 
-        frameRef.current?.contentWindow.postMessage(
-          {
-            type: 'highlight-element-start', payload: {index: element}
-          },
-          window.location.origin,
-        );
-      }}
-      onMouseLeave={() => {
-        if (element?.classList) {
-          removeHighlight(element);
-          return;
-        }
+          frameRef.current?.contentWindow.postMessage(
+            {
+              type: 'highlight-element-start', payload: {index: element}
+            },
+            window.location.origin,
+          );
+        }}
+        onMouseLeave={() => {
+          if (element?.classList) {
+            removeHighlight(element);
+            return;
+          }
 
-        frameRef.current?.contentWindow.postMessage(
-          {
-            type: 'highlight-element-end', payload: {index: element}
-          },
-          window.location.origin,
-        );
-      }}
-    >
-      {isRootElement ? <span style={{float: 'right'}}>global</span> : <ScrollInViewButton {...{element}}/>}
-      
-      <h4
-        style={{fontWeight: 400, marginBottom: 0, paddingRight: '4px',cursor: 'pointer', display: 'flex', justifyContent: 'space-between'}}
-        onClick={() => toggleGroup(label)}
+          frameRef.current?.contentWindow.postMessage(
+            {
+              type: 'highlight-element-end', payload: {index: element}
+            },
+            window.location.origin,
+          );
+        }}
       >
-        <div>
-          {label} ({vars.length})
-          {propertyFilter !== 'all' && <span style={{color: 'grey', fontSize: '12px'}}
-          >{propertyFilter}</span>}
-          { search !== '' && <span style={{color: 'grey', fontSize: '12px'}}
-          >
-            - "{search}"
-            <button
-              style={{
-                fontSize: '7px',
-                padding: '3px 3px 1px',
-                position: 'relative',
-                bottom: '4px',
-                borderColor: 'grey'
-                }}
-              title="Clear search"
-              onClick={() => { setSearch('') }}
-            >X</button>
-            </span>}
-          {groupColors.length > 0 && <ul style={{listStyleType: 'none', display: 'inline-flex', margin: 0}}>
-            {groupColors.map(([{name}, value]) => {
-              const isVar = name.startsWith('--');
-              return <div
-                draggable
-                onDragStart={e=>e.dataTransfer.setData('value', value)}
-                key={name}
-                title={`${name}: ${value}`}
-                style={{
-                  display: 'inline-block',
-                  width: previewSize,
-                  height: previewSize,
-                  lineHeight: '1.5',
-                  border: '1px solid black',
-                  borderRadius: '6px',
-                  backgroundImage: `${value}`,
-                  backgroundColor: `${value}`,
-                  backgroundRepeat: `no-repeat`,
-                  backgroundSize: 'cover',
-                  marginTop: '7px',
-                  marginLeft: '6px',
-                  fontSize: '14px',
-                  textAlign: 'center',
-                  textShadow: isVar ? 'white 0px 3px' : 'white 2px 2px'
-                }}>{/^var\(/.test(value) ? 'v' : value === 'transparent' ? '👻' : ! isVar ? 'r': <Fragment>&nbsp;</Fragment>}</div>
-              })}
-          </ul>}
-        </div>
-
-        {elSrc && <img src={elSrc} srcSet={elSrcset} alt={elAlt} title={elTitle || elAlt} style={{height: '52px', float: 'right', backgroundColor: 'grey'}}/>}
-        {elHtml?.length > 0 && <div
-          className='svg-inspect-wrapper'
-          style={{display: 'inline', position: 'relative', minWidth: `${elWidth}px`, maxWidth: '50%', maxHeight: '160px', outline: '1px solid grey', padding: '2px', background: darkSvg ? 'black' : 'transparent'}}
-          onClick={(e) => {setDarkSvg(!darkSvg); e.stopPropagation()}}
-          dangerouslySetInnerHTML={{__html: elHtml}}
-        ></div>}
-        {group.inlineStyles && <span style={{...{border: '1px solid black'}, ...group.inlineStyles, ...{maxHeight: previewSize, width: 'auto'}}}>Inline</span>}
+        {isRootElement ? <span style={{float: 'right'}}>global</span> : <ScrollInViewButton {...{element}}/>}
         
-      </h4>
-    </div>
-    {isOpen && <Fragment>
-      <ElementInlineStyles {...{group, elementScopes}}/>
-      <ScopeControl {...{scopes: elementScopes, vars, element}}/>
-      <ul className={'group-list'}>
-        {vars.filter(v=>!v.currentScope).map(cssVar => {
-          return <VariableControl
-            {...{
-              cssVar,
-              scopes: elementScopes,
-              element,
-            }}
-            initialOpen={vars.length === 1}
-            key={cssVar.name}
-            onChange={value => {
-              dispatch({
-                type: ACTIONS.set,
-                payload: {
-                  name: cssVar.name, 
-                  value,
-                }
-              });
-            }}
-            onUnset={() => {
-              dispatch({ type: ACTIONS.unset, payload: { name: cssVar.name } });
-            }}
-          />;
-        }
-        )}
-      </ul>
-    </Fragment>}
-  </li>;
+        <h4
+          style={{fontWeight: 400, marginBottom: 0, paddingRight: '4px',cursor: 'pointer', display: 'flex', justifyContent: 'space-between'}}
+          onClick={() => toggleGroup(label)}
+        >
+          <div>
+            {label} ({vars.length})
+            {propertyFilter !== 'all' && <span style={{color: 'grey', fontSize: '12px'}}
+            >{propertyFilter}</span>}
+            { search !== '' && <span style={{color: 'grey', fontSize: '12px'}}
+            >
+              - "{search}"
+              <button
+                style={{
+                  fontSize: '7px',
+                  padding: '3px 3px 1px',
+                  position: 'relative',
+                  bottom: '4px',
+                  borderColor: 'grey'
+                  }}
+                title="Clear search"
+                onClick={() => { setSearch('') }}
+              >X</button>
+              </span>}
+            {groupColors.length > 0 && <ul style={{listStyleType: 'none', display: 'inline-flex', margin: 0}}>
+              {groupColors.map(([{name}, value]) => {
+                const isVar = name.startsWith('--');
+                return (
+                  <div
+                    draggable
+                    onDragStart={e=>e.dataTransfer.setData('value', value)}
+                    key={name}
+                    title={`${name}: ${value}`}
+                    style={{
+                      display: 'inline-block',
+                      width: previewSize,
+                      height: previewSize,
+                      lineHeight: '1.5',
+                      border: '1px solid black',
+                      borderRadius: '6px',
+                      backgroundImage: `${value}`,
+                      backgroundColor: `${value}`,
+                      backgroundRepeat: `no-repeat`,
+                      backgroundSize: 'cover',
+                      marginTop: '7px',
+                      marginLeft: '6px',
+                      fontSize: '14px',
+                      textAlign: 'center',
+                      textShadow: isVar ? 'white 0px 3px' : 'white 2px 2px'
+                    }}>{/^var\(/.test(value) ? 'v' : value === 'transparent' ? '👻' : ! isVar ? 'r': <Fragment>&nbsp;</Fragment>}</div>
+                );
+                })}
+            </ul>}
+          </div>
+
+          {elSrc && <img src={elSrc} srcSet={elSrcset} alt={elAlt} title={elTitle || elAlt} style={{height: '52px', float: 'right', backgroundColor: 'grey'}}/>}
+          {elHtml?.length > 0 && <div
+            className='svg-inspect-wrapper'
+            style={{display: 'inline', position: 'relative', minWidth: `${elWidth}px`, maxWidth: '50%', maxHeight: '160px', outline: '1px solid grey', padding: '2px', background: darkSvg ? 'black' : 'transparent'}}
+            onClick={(e) => {setDarkSvg(!darkSvg); e.stopPropagation()}}
+            dangerouslySetInnerHTML={{__html: elHtml}}
+          ></div>}
+          {group.inlineStyles && <span style={{...{border: '1px solid black'}, ...group.inlineStyles, ...{maxHeight: previewSize, width: 'auto'}}}>Inline</span>}
+          
+        </h4>
+      </div>
+      {isOpen && <Fragment>
+        <ElementInlineStyles {...{group, elementScopes}}/>
+        <ScopeControl {...{scopes: elementScopes, vars, element}}/>
+        <ul className={'group-list'}>
+          {vars.filter(v=>!v.currentScope).map(cssVar => {
+            return <VariableControl
+              {...{
+                cssVar,
+                scopes: elementScopes,
+                element,
+              }}
+              initialOpen={vars.length === 1}
+              key={cssVar.name}
+              onChange={value => {
+                dispatch({
+                  type: ACTIONS.set,
+                  payload: {
+                    name: cssVar.name, 
+                    value,
+                  }
+                });
+              }}
+              onUnset={() => {
+                dispatch({ type: ACTIONS.unset, payload: { name: cssVar.name } });
+              }}
+            />;
+          }
+          )}
+        </ul>
+      </Fragment>}
+    </li>
+  );
 };
