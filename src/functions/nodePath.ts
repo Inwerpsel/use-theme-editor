b9e45ad266122029f9d9d710c913de45015ddd0b
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-03-03 16:06:33 +0100

    Capture deepest id in node path and find node from there

--- src/functions/nodePath.ts
+++ src/functions/nodePath.ts
@@ -5,25 +5,51 @@ function getIndex(node: HTMLElement, parent: HTMLElement) {
 // Generate a path that can be used to find the node if the relevant document structure hasn't changed.
 export function toPath(node) {
     const path = [];
+    // Only collected deepest id for now.
+    let didId = false;
     while (node !== document.body) {
         const parent = node.parentNode;
-        path.push([node.tagName, getIndex(node, parent)]);
+        const entry = [node.tagName, getIndex(node, parent)];
+        if (!didId && node.id) {
+            entry.push(node.id);
+        }
+        path.push(entry);
         node = parent;
     }
-    return path;
+    return path.reverse();
 }
 
 // Find node from path.
-export function toNode(path) {
+export function toNode(path: string[]) {
     let node = document.body;
-    for (const [tagname, position] of [...path].reverse()) {
-        node = node.children[position];
-        if (!node) {
+    let i = 0;
+    let deepestIdIndex;
+    for (const [,,id] of path) {
+        if (id) {
+            deepestIdIndex = i;
+        }
+        i++;
+    }
+
+    if (deepestIdIndex) {
+        const elById = document.getElementById(path[deepestIdIndex][2]);
+        if (elById) {
+            // Start from deepest specified id if it exists.
+            node = elById;
+            path = path.slice(deepestIdIndex + 1);
+        }
+    }
+
+    for (const [tagname, position] of path) {
+        const next = Array.from(node.children)[position];
+        if (!next) {
             throw new Error(`No element at index ${position}`);
         }
+        node = next;
         if (node.tagName !== tagname) {
             console.warn(`Expected ${tagname} but found ${node.tagName}`)
         }
     }
+
     return node;
 }
