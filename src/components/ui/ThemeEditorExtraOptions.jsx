e06b7795cfb875d42fca74fd23aacf9968d9335e
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-04-15 16:55:39 +0200

    Improve inspection behavior toggle and add info using title

--- src/components/ui/ThemeEditorExtraOptions.jsx
+++ src/components/ui/ThemeEditorExtraOptions.jsx
@@ -5,14 +5,21 @@ import { Tutorial } from '../../_unstable/Tutorial';
 export function ThemeEditorExtraOptions() {
   const [frameClickBehavior, setFrameClickBehavior] = use.frameClickBehavior();
 
+  const useAlt = frameClickBehavior === 'alt';
+
   return <div>
     <Tutorial el={ThemeEditorExtraOptions}>Toggle whether a normal click performs an inspection and blocks default click behavior.</Tutorial>
     <button
+      title={
+        useAlt 
+        ? 'Clicks are processed by the page and should result in normal responses.\n You can still inspect by holding the ALT key while clicking.' 
+        : 'Clicking on an element will inspect it.\n Most normal behavior is prevented (e.g. links), but some may still get triggered (e.g. accordion state).'
+      }
       onClick={() => {
         setFrameClickBehavior(frameClickBehavior === 'alt' ? 'any' : 'alt');
       }}
     >
-      {frameClickBehavior === 'alt' ? 'Require ALT for inspect (ON)' : 'Require ALT for inspect (OFF)'}
+      {useAlt ? 'Cursor: interact' : 'Cursor: inspect'}
     </button>
   </div>;
 }
