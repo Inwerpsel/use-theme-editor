bbbd9bd239bfa214507e7f192e284a75a8658344
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-07-02 19:16:18 +0200

    Auto detect prefixes in variable names

--- src/components/inspector/RemoveAnnoyingPrefix.jsx
+++ src/components/inspector/RemoveAnnoyingPrefix.jsx
@@ -1,11 +1,89 @@
-import React  from "react";
+import React, { useContext, useEffect, useState }  from "react";
 import { get, use } from "../../state";
 import { TextControl } from "../controls/TextControl";
 import { Tutorial } from "../../_unstable/Tutorial";
 import { DragHandle } from "../movable/DragHandle";
+import { scopesByProperty } from "../../functions/collectRuleVars";
+import { ThemeEditorContext } from "../ThemeEditor";
+import { Checkbox } from "../controls/Checkbox";
+
+const minPrefixAmount = 4;
+
+function getPossiblePrefixes(name) {
+  const parts = name.replace(/^--/, '').split('-');
+  const prefixes = [];
+
+  for (let i = 0; i < 3 && i < parts.length; i++) {
+    prefixes.push(parts.slice(0, i + 1).join(' '));
+  }
+
+  return prefixes;
+}
+
+function detectPrefixes(allVars) {
+  const start = performance.now();
+  const allPrefixes = new Map();
+  function initPrefix(str) {
+    if (!allPrefixes.has(str)) {
+      allPrefixes.set(str, new Set());
+    }
+    return allPrefixes.get(str);
+  }
+  // All prefixes found more than min, sorted by occurrences
+  for (const cssVar of allVars) {
+    if (!cssVar.name.startsWith('--')) {
+      continue;
+    }
+    for (const prefix of getPossiblePrefixes(cssVar.name)) {
+      const set = initPrefix(prefix.trim());
+      set.add(cssVar.name);
+    }
+  }
+
+  for (const name of Object.keys(scopesByProperty)) {
+    for (const prefix of getPossiblePrefixes(name)) {
+      const set = initPrefix(prefix.trim());
+      set.add(name);
+    }
+  }
+
+  const filtered = [...allPrefixes.entries()].filter(([, set]) => set.size > minPrefixAmount);
+
+  const sorted = filtered.sort(([,setA], [,setB]) => setB.size - setA.size);
+
+  console.log(sorted);
+  console.log('time', performance.now() - start);
+  return sorted;
+}
 
 export function RemoveAnnoyingPrefix() {
+  const {
+    allVars,
+  } = useContext(ThemeEditorContext);
+
+  const [detectionResult, setDetectionResult] = useState(null);
+  const [multiMode, setMultiMode] = useState(false);
+  const [nResults, setNResults] = useState(5);
   const [annoyingPrefix, setAnnoyingPrefix] = use.annoyingPrefix();
+  const [multiSelection, setMultiSelection] = useState(new Set());
+
+  function addToMultiSelect(prefix) {
+    multiSelection.has(prefix) ? multiSelection.delete(prefix) : multiSelection.add(prefix);
+    setMultiSelection(new Set(multiSelection));
+  }
+
+  function confirmMultiSelection() {
+    setAnnoyingPrefix(`${[...multiSelection.values()].join('|')}`)
+    // setDetectionResult(null);
+  }
+
+  function setPrefixAndCleanup(prefix) {
+    setAnnoyingPrefix(prefix);
+    // setDetectionResult(null);
+  }
+  // useEffect(() => {
+  //   setDetectionResult(detectPrefixes(allVars));
+  // }, [])
 
   return <div>
     <Tutorial
@@ -31,6 +109,34 @@ export function RemoveAnnoyingPrefix() {
         textDecorationThickness: '1px',
       }}
     />
+    {!detectionResult && <button onClick={() => {
+      setDetectionResult(detectPrefixes(allVars))
+    }}>Detect</button>}
+    {detectionResult && <button onClick={() => {setDetectionResult(null)}}>Close</button>}
+      
+    {detectionResult && <div>
+      <input
+        type="number"
+        min="1"
+        value={nResults}
+        onChange={e => {setNResults(e.target.value)}}
+      />
+      <Checkbox controls={[multiMode, setMultiMode]}>Multi</Checkbox>
+      {multiMode && multiSelection?.size > 0 && <button onClick={confirmMultiSelection}>Confirm multi select</button>}
+      <button onClick={() => {setAnnoyingPrefix('')}}>None</button>
+      {detectionResult.slice(0, nResults).map( ([prefix, set]) => <div key={prefix}>
+        <button
+          style={prefix === annoyingPrefix 
+            ? {background: 'lightblue'}
+            : multiMode && multiSelection.has(prefix)
+            ? {background: 'yellow'}
+            : {}
+          }
+          onClick={(multiMode ? addToMultiSelect : setPrefixAndCleanup).bind(null, prefix)}
+          title={[...set.values()].join('\n')}
+        >{multiMode && multiSelection.has(prefix) && <input type="checkbox" checked/>}{prefix} ({set.size})</button>
+      </div>)}
+    </div>}
   </div>;
 }
 
