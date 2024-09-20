21268a69284eb9802871395599aeb68e8158158b
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-09-20 16:20:30 +0200

    Clip allowed ranges, fix incorrect chroma gradient
    The picker should now produce (very close to) exactly the same result as the online picker.
    
    A link to this picker is also added using url params.
    
    The only thing "missing" is clipping the gaps in allowed hues for the other settings.
    
    The previously used gradient for chroma was roughly correct,
    but the lightness values didn't align along the way.
    The gradient was made from gray to the fullest chroma color with no stops, which caused it to shift into other colors.
    I guess it's because it's walking a straight line, where actually the color space has a detour between those colors. Or visce versa.
    
    To fix this, I added color stops until I was sure the result was unnoticeable. This seems to not be too heavy to render.

--- src/components/properties/OklchColorControl.tsx
+++ src/components/properties/OklchColorControl.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { converter, clampChroma  } from 'culori';
+import { converter, clampChroma, parse  } from 'culori';
 
 
 const okConv = converter('oklch');
@@ -9,12 +9,90 @@ function extract(value) {
 
 }
 
+const noLightness = 0;
+const fullLightness = 1;
+const bigStep = 0.01;
+const smallStep = 0.001;
+
+function minLightness(c, h) {
+  let smallestDiff = Infinity;
+  let l = noLightness, parsed = parse(`oklch(${l}% ${c} ${h})`);
+  while (l < fullLightness) {
+    l += bigStep;
+    const diff = Math.abs(clampChroma({...parsed, l}, 'oklch', 'p3').c - c);
+    if (diff === 0) {
+      return l * 100;
+    }
+    // The diff should only decrease until we land in or after the allowed range.
+    if (diff >= smallestDiff) {
+      break;
+    }
+  }
+
+  const tooMuch = l;
+  l -= bigStep;
+  smallestDiff = Infinity;
+
+  while (l < tooMuch) {
+    l += smallStep;
+    const diff = Math.abs(clampChroma({...parsed, l}, 'oklch', 'p3').c - c);
+    if (diff === 0) {
+      return l;
+    }
+    if (diff >= smallestDiff) {
+      break;
+    }
+  }
+  return l * 100;
+}
+
+function maxLightness(c, h) {
+  let smallestDiff = Infinity;
+  let l = fullLightness, parsed = parse(`oklch(${l}% ${c} ${h})`);
+  while (l > noLightness) {
+    l -= bigStep;
+    const diff = Math.abs(clampChroma({...parsed, l}, 'oklch', 'p3').c - c);
+    if (diff === 0) {
+      return l * 100;
+    }
+    if (diff >= smallestDiff) {
+      break;
+    }
+
+  }
+
+  const tooLittle = l;
+  l -= bigStep;
+  smallestDiff = Infinity;
+
+  while (l > tooLittle) {
+    l -= smallStep;
+    const diff = Math.abs(clampChroma({...parsed, l}, 'oklch', 'p3').c - c);
+    if (diff === 0) {
+      return l;
+    }
+    if (diff >= smallestDiff) {
+      break;
+    }
+  }
+  return l * 100;
+}
+
+function OnlinePickerLink({l, c, h, a = 100}) {
+  // https://oklch.com/#50,0.1574,130,100
+  return <a href={`https://oklch.com/#${l},${c},${h},${a}`} target='_blank'>online picker</a>
+}
+
 export function OklchColorControl({value, onChange}) {
     const { l: _l, c, h: _h } = extract(value) || { l: 0, c: 0, h: 0 };
     const l = 100 * _l;
     const h = !_h ? 0 : _h;
-    const clamped = clampChroma(`oklch(${l}% 0.4 ${h})`);
-    const maxChroma = clamped.c > 0 ? clamped.c : 0.4;
+    const clamped = clampChroma(`oklch(${l}% 0.4 ${h})`, 'oklch', 'p3');
+    const maxChroma = clamped.c; 
+    const isNotInGamut = maxChroma > c;
+    const lowerL = minLightness(c, h);
+    const upperL = maxLightness(c, h);
+    // console.log(lower);
 
     return (
       <div className="oklch-picker" style={{
@@ -22,10 +100,22 @@ export function OklchColorControl({value, onChange}) {
         '--picked-chroma': c,
         '--picked-hue': h,
         '--max-chroma': maxChroma,
+        '--min-lightness': `${lowerL}%`,
+        '--max-lightness': `${upperL}%`,
+        '--max-lightness-scalar': upperL,
       }}>
-        <div className="lightness">
+        <div className="lightness" onDrop={e=>{
+          const value = e.dataTransfer.getData('value');
+          const {l} = extract(value);
+          if (l > 0) {
+            e.preventDefault();
+            e.stopPropagation();
+            onChange(`oklch(${l * 100}% ${c.toFixed(3)} ${h.toFixed(2)})`)
+          }
+        }}>
           <input onChange={e=>onChange(`oklch(${e.target.value}% ${c.toFixed(3)} ${h.toFixed(2)})`)} id="lightness" type="range" min={0} max={100} value={l} />
         </div>
+        {/* <div className="chroma test"></div> */}
         <div className="chroma">
           <input
             id="chroma"
@@ -40,9 +130,18 @@ export function OklchColorControl({value, onChange}) {
             }} 
           />
         </div>
-        <div className="hue">
+        <div className="hue" onDrop={e=>{
+          const value = e.dataTransfer.getData('value');
+          if (!value) return;
+          const {h} = extract(value);
+          e.preventDefault();
+          e.stopPropagation();
+          onChange(`oklch(${l}% ${c.toFixed(3)} ${h.toFixed(2)})`)
+        }}>
           <input id="hue" type="range" min={0} max={360} value={h}onChange={e=>onChange(`oklch(${l.toFixed(2)}% ${c.toFixed(3)} ${e.target.value})`)}  />
         </div>
+        <OnlinePickerLink {...{l,c,h}} />
+        {!isNotInGamut && <span style={{color: 'red'}}>NOT IN GAMUT</span>}
       </div>
     );
 }
