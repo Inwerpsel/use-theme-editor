bc6581f259f2c4e28c90405a40bc9ef6fdba1e6f
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-10-03 15:50:34 +0200

    WIP Allow full screen of preview only, use portal "hack" for exit button
    Apparently you can stack multiple full screen elements, at which point
    closing them gets a bit weird.

---
+++ src/components/ui/FullscreenToggle.tsx
@@ -0,0 +1,44 @@
+import React, { Fragment, useContext, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { ThemeEditorContext } from '../ThemeEditor';
+
+function toggleFullscreen() {
+  if (document.fullscreenElement !== this) {
+    this.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+export function FullscreenToggle() {
+    const {
+      frameRef,
+    } = useContext(ThemeEditorContext);
+
+    const [isFullscreenPreview, setIsFullscreenPreview] = useState(false);
+
+    const previewButton = (
+      <button
+        onClick={() => {
+          const node = frameRef.current?.parentNode;
+          setIsFullscreenPreview(document.fullscreenElement !== node);
+          toggleFullscreen.bind(node)();
+        }}
+      >
+        fullscreen preview
+      </button>
+    );
+
+    const buttons = (
+      <Fragment>
+        <button onClick={toggleFullscreen.bind(document.body)}>
+          fullscreen
+        </button>
+        {previewButton}
+      </Fragment>
+    );
+    return <Fragment>
+      {buttons}
+      {isFullscreenPreview && createPortal(<div style={{ position: 'fixed', top: 0, right: 0}}>{previewButton}</div>, frameRef.current?.parentNode)}
+    </Fragment>
+}
