f49ee150138a65c567a1643f972e4ce283005fba
Pieter Vincent <pieter.vincent1988@gmail.com>
2024-12-24 15:27:03 +0100

    Expand selectors widget

--- src/components/ui/Selectors.jsx
+++ src/components/ui/Selectors.jsx
@@ -1,6 +1,50 @@
 import { ElementLocator } from "./ElementLocator";
 import { use } from "../../state";
-import { Fragment, useRef, useState } from "react";
+import { useContext, useMemo, useState } from "react";
+import { ToggleButton } from "../controls/ToggleButton";
+import { TextControl } from "../controls/TextControl";
+import { ThemeEditorContext } from "../ThemeEditor";
+import { useLocalStorage } from "../../hooks/useLocalStorage";
+
+// let isCaptureMode = false, params = {};
+
+// function param(name) {
+//   if (isCaptureMode) {
+//     // Record that there is a param, so we can show input for it.
+//     params[name] = null;
+//   } else {
+//     return params[name];
+//   }
+// }
+
+// function captureParams(snippet) {
+//   params = {};
+//   isCaptureMode = true;
+
+//   snippet();
+
+//   isCaptureMode = false;
+//   return params;
+// }
+
+// function evaluateParams(snippet, p) {
+//   params = p;
+// }
+
+// const snippets = {
+//   'Elements with inline style': () => `[style]`,
+//   'Elements with inline custom properties': () => `[style*=--]`,
+//   'Elements with a specific inline custom property': ({name}) => `[style*=${name}:]`,
+// };
+
+// function SnippetUI() {
+//   return <div>
+//     {snippets.map((snippet) => {
+//       const params = captureParams(snippet);
+//       return <div></div>;
+//     })}
+//   </div>
+// }
 
 function DropAction({ onDrop, children }) {
   return (
@@ -46,22 +90,27 @@ function DropCombineSelector({selector, add, shown}) {
 }
 
 function Selector({ selector, add, remove }) {
-  const ref = useRef();
+  // const ref = useRef();
   const [showDropTargets, setShowDropTargets] = useState(false);
   return (
     <div
       // onDragEnter={(e) => {
-      //   ref.current && clearTimeout(ref.current);
       //   setShowDropTargets(true);
+      //   ref.current && clearTimeout(ref.current);
+      //   ref.current = setTimeout(() => {
+      //     setShowDropTargets(true);
+      //   }, 20);
       //   e.preventDefault();
       // }}
       // onDragLeave={(e) => {
-      //   ref.current = setTimeout(() => {
-      //     setShowDropTargets(false);
-      //   }, 100);
+      //   // ref.current && clearTimeout(ref.current);
+      //   setShowDropTargets(false);
+      //   // ref.current = setTimeout(() => {
+      //   //   setShowDropTargets(false);
+      //   // }, 20);
       // }}
     >
-      {/* <DropCombineSelector {...{ selector, add, shown: showDropTargets }} /> */}
+      <DropCombineSelector {...{ selector, add, shown: showDropTargets }} />
       <button style={{ float: 'right' }} onClick={() => remove(selector)}>
         -
       </button>
@@ -71,8 +120,22 @@ function Selector({ selector, add, remove }) {
 }
 
 export function Selectors() {
-    // Todo: support set in local storage.
+    // Todo: support Set in local storage.
     const [selectors, setSelectors] = use.savedSelectors();
+    const { frameRef } = useContext(ThemeEditorContext);
+
+    const [open, setOpen] = useLocalStorage('showSelectorsWidget', true);
+    const [addMode, setAddMode] = useState(false);
+    const [newSelector, setNewSelector] = useState('');
+
+    const [isValid, amount] = useMemo(() => {
+      try {
+        return [true, frameRef.current?.contentWindow.document.querySelectorAll(newSelector).length]
+      } catch (e) {
+        // console.log(e);
+        return [false];
+      }
+    }, [newSelector])
 
     function add(selector) {
         if (selectors.includes(selector)) {
@@ -87,23 +150,35 @@ export function Selectors() {
         }
         setSelectors(selectors.filter(s=>s!==selector));
     }
+    const onDrop = (e) => {
+      const text = e.dataTransfer.getData('text/plain').trim();
+      try {
+        // if ((text?.trim() || '') === '') throw new Error('hmm');
+        document.querySelector(text);
+        add(text);
+      } catch (err) {
+        const selector = e.dataTransfer.getData('selector');
+        if (selector) {
+          add(selector);
+        }
+      }
+    }
+
+    if (!open) {
+      return <button
+        onDragOver={e=>e.preventDefault()}
+        onDrop={(e) => {
+          onDrop(e);
+          setOpen(true);
+        }}
+        onClick={() => {setOpen(true)}}
+      >Selectors...</button>
+    }
 
     return (
       <div
         onDragOver={e=>e.preventDefault()}
-        onDrop={(e) => {
-          const text = e.dataTransfer.getData('text/plain').trim();
-          try {
-            // if ((text?.trim() || '') === '') throw new Error('hmm');
-            document.querySelector(text);
-            add(text);
-          } catch (err) {
-            const selector = e.dataTransfer.getData('selector');
-            if (selector) {
-              add(selector);
-            }
-          }
-        }}
+        {...{onDrop}}
       >
         {selectors.length === 0 && <h2>Drop selectors here</h2>}
         <ul>
@@ -113,6 +188,13 @@ export function Selectors() {
             </li>
           ))}
         </ul>
+        <button onClick={() => {setOpen(false)}}>Close</button>
+        <ToggleButton controls={[addMode, setAddMode]}>New...</ToggleButton>
+        {addMode && <div>
+          <TextControl {...{value: newSelector, onChange: setNewSelector}} />
+          <button {...{disabled: newSelector === '', onClick: () => {add(newSelector)}}}>Add</button>
+          {isValid && <span>Found {amount}</span>}
+        </div>}
       </div>
     );
 }
